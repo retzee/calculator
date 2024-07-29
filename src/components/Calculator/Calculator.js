a0644babe89c3fb7f192e1c49440d79ab7f4be07
Sam Odele <odelesamson@gmail.com>
2024-07-29 23:36:26 +0100

    App update

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -2,10 +2,8 @@ import React from "react";
 
 import styles from './Calculator.module.css';
 
-const Calculator = (props) => {
+export default Calculator = (props) => {
     return (
         <div className={styles.calculator}>{props.children}</div>
     );
 }
-
-export default Calculator;
