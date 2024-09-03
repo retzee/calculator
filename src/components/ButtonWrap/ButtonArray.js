cb56e6e9c2d8ac2adca5ca96c3175d64424b6e5a
Sam Odele <odelesamson@gmail.com>
2024-09-03 23:20:39 +0100

    update

--- src/components/ButtonWrap/ButtonArray.js
+++ src/components/ButtonWrap/ButtonArray.js
@@ -1,5 +1,5 @@
 const btnList = ['+', '-', 'x', '/', 7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.', 'AC', '=']; // All button list
-const operatorList = ['+', '-', 'x', '/']; //All Operator buttons
-const numberList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; //All Numeric buttons
+const operatorList = ['+', '-', 'x', '/']; // All Computation Operator buttons
+const numberList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; // All Numeric buttons
 
 export {btnList, numberList, operatorList};
