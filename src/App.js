e056777f7f7fb46459e97c3f92f82ccfd9cc7e94
Sam Odele <odelesamson@gmail.com>
2024-10-11 23:20:29 +0100

    update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     keyCategory = Number(keyCategory);
     
     if(!outputString && outputResult){
-      setOutputResult(0); //Reset Output Result to zero after completed computation
+      setOutputResult(0); //Reset Output Result to zero after computation
     }
 
     let lastCharCategory = outputStrLastCharCategory();
