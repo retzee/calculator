39076dc5ba040502a42cc8a67ff7c44de48539af
Sam Odele <odelesamson@gmail.com>
2025-01-28 22:27:23 +0100

    Update

--- src/components/ButtonWrap/ButtonWrap.js
+++ src/components/ButtonWrap/ButtonWrap.js
@@ -23,7 +23,7 @@ const ButtonWrap = (props) => {
                         getBtnCategory = 2;
                     }
                     else if(element === "="){
-                        classValue = `${styles.calculator__key} ${styles.calculator__key__enter}`;
+                        className = `${styles.calculator__key} ${styles.calculator__key__enter}`;
                         getBtnCategory = 3;
                     }
                     else{
